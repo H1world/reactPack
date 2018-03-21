56b882294db861d18c32e61a270dd31dcb2eb9e3
gyf <462293253@qq.com>
2018-03-21 14:01:54 +0800

    react-pack

---
+++ env.js
@@ -0,0 +1,4 @@
+module.exports = (env) => {
+  let customStuff = env.customStuff;
+  console.log(customStuff);
+};
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './style.less';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div>213</div>
+    );
+  }
+}
